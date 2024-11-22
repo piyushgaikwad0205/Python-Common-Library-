c6569df2bb75cd853d36bd8793414f98ceb25014
piyushgaikwad0205 <gaikawadpiyush488@gmail.com>
2024-11-23 03:26:03 +0530

    First commit

--- index.py
+++
@@ -1,3 +0,0 @@
-from myLib.dataValidation import Validation
-
-print(Validation.String('y', 'n',message="Enter between y or n\nYour Choise: " , error="\noh.. this is an Error. Try Again\n"))
--- lamda.py
+++
@@ -1,3 +0,0 @@
-list = ['y', 'n']
-
-print(list.lower())
